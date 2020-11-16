a409564640a1f37205e63490d7296297ad6bc1cf
ajisahputra78 <ajisaputro.s.kom@gmail.com>
2020-11-17 03:41:29 +0700

    Initial Commit

---
+++ LocationSpeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationSpeed
+//
+//  Created by Aji_sahputra on 17/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
